6b9f614a97c80f066d27b34481e3d19b60068916
RNikita123t <9752rathorenikita@gmail.com>
2024-07-14 17:10:23 +0530

    made change in congif

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    secret: 'secret',
-    mongoUri: 'mongodb+srv://nikitarathore271201:LA3XTUPvDbQ0cZ0S@cluster0.dlvvg0o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    secret: '',
+    mongoUri: '',
   };
   
